33f14005a29f772275fa726889ca48f6f65d3ac0
geralddcx <gerald.ducloux@gmail.com>
2022-04-07 17:51:21 +0200

    quest 3

---
+++ cypress/integration/test_api_tastedive.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+describe('essai', () => {
+    const NewSearch = require('../fixtures/tasteDiveData.json');
+
+    it('cas pour ABBA et une limite de 10', () => {
+        cy.log(NewSearch);
+
+        cy.similarSearchQ(NewSearch[0])
+        .then( result => {
+            cy.log(JSON.stringify(result));
+            expect(result).to.exist;
+            expect(result.body.Similar).to.exist;
+            expect(result.body.Similar.Info.length).to.be.gt(0);
+           // expect(result.body.Similar.Info[0].Name).to.contain('ABBA');
+//expect(result.body.Similar.Results.length).to.be.eq(NewSearch[0].limit);
+           expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[0].limit);
+        })
+    })
+    it('cas pour The cardigans et une limite de 15', () => {
+        cy.log(NewSearch);
+
+        cy.similarSearchQ(NewSearch[1])
+        .then( result => {
+            cy.log(JSON.stringify(result));
+            expect(result).to.exist;
+            expect(result.body.Similar).to.exist;
+            expect(result.body.Similar.Info.length).to.be.gt(0);
+            // expect(result.body.Similar.Info[0].Name).to.deep.equal('ABBA');
+//expect(result.body.Similar.Results.length).to.be.eq(NewSearch[1].limit);
+            expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[1].limit);
+        })
+    });
+
+    it('cas pour Balavoine et une limite de 20', () => {
+        cy.log(NewSearch);
+
+        cy.similarSearchQ(NewSearch[2])
+        .then( result => {
+            cy.log(JSON.stringify(result));
+            expect(result).to.exist;
+            expect(result.body.Similar).to.exist;
+            expect(result.body.Similar.Info.length).to.be.gt(0);
+            // expect(result.body.Similar.Info[0].Name).to.deep.equal('ABBA');
+          //  expect(result.body.Similar.Results.length).to.be.eq(NewSearch[2].limit);
+            expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[2].limit);
+        })
+    })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('countriesCoronaSearch', (query, page, limit) => {
+    cy.request({
+        url: 'https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search',
+        qs: {
+            search: query,
+            page: page,
+            limit: limit
+        }
+    }) 
+}) 
+
+Cypress.Commands.add('similarSearch', (sName, sType, nInfo, nLimit) => {
+    cy.request('https://tastedive.com/api/similar?q='+sName+'&type='+sType+'&limit='+nLimit+'&info='+nInfo+'&k=435163-testapi-GMXUXISO');     
+}) 
+
+Cypress.Commands.add('similarSearchQ', (query) => {
+    cy.request(`https://tastedive.com/api/similar?q=${query.name}&type=${query.type}&limit=${query.limit}&info=${query.info}&k=435163-testapi-GMXUXISO`);     
+}) 

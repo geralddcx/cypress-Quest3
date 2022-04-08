616e66795121bf293e82d434590f4599e1a50be2
geralddcx <gerald.ducloux@gmail.com>
2022-04-08 13:49:03 +0200

    ajout à tous les tests des doubles fonctions

--- cypress/integration/test_api_tastedive.spec.js
+++ cypress/integration/test_api_tastedive.spec.js
@@ -27,6 +27,7 @@ describe('essai', () => {
             expect(result.body.Similar).to.exist;
             expect(result.body.Similar.Info.length).to.be.gt(0);
             expect(result.body.Similar.Info[0].Name.toLowerCase()).to.be.equal(NewSearch[0].name.toLowerCase());
+            expect(result.body.Similar.Info[0].Name.toLowerCase()).contains(NewSearch[0].name.toLowerCase());
 //expect(result.body.Similar.Results.length).to.be.eq(NewSearch[0].limit);
             expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[0].limit);
         })
@@ -45,6 +46,20 @@ describe('essai', () => {
 //expect(result.body.Similar.Results.length).to.be.eq(NewSearch[1].limit);
             expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[1].limit);
         })
+
+        cy.similarSearchN(NewSearch[1].name,NewSearch[1].type, NewSearch[1].info, NewSearch[1].limit)
+        .then( result => {
+            cy.log(JSON.stringify(result));
+            cy.log("1" + NewSearch[1].name);
+            cy.log("2" + result.body.Similar.Info[0].Name.toLowerCase());
+            expect(result).to.exist;
+            expect(result.body.Similar).to.exist;
+            expect(result.body.Similar.Info.length).to.be.gt(0);
+            expect(result.body.Similar.Info[0].Name.toLowerCase()).to.be.equal(NewSearch[1].name.toLowerCase());
+            expect(result.body.Similar.Info[0].Name.toLowerCase()).contains(NewSearch[1].name.toLowerCase());
+//expect(result.body.Similar.Results.length).to.be.eq(NewSearch[0].limit);
+            expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[1].limit);
+        })
     });
 
     it('cas pour Balavoine et une limite de 20', () => {
@@ -58,6 +73,19 @@ describe('essai', () => {
             expect(result.body.Similar.Info.length).to.be.gt(0);
             // expect(result.body.Similar.Info[0].Name).to.deep.equal('ABBA');
           //  expect(result.body.Similar.Results.length).to.be.eq(NewSearch[2].limit);
+            expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[2].limit);
+        })
+        cy.similarSearchN(NewSearch[2].name,NewSearch[2].type, NewSearch[2].info, NewSearch[2].limit)
+        .then( result => {
+            cy.log(JSON.stringify(result));
+            cy.log("1" + NewSearch[2].name);
+            cy.log("2" + result.body.Similar.Info[0].Name.toLowerCase());
+            expect(result).to.exist;
+            expect(result.body.Similar).to.exist;
+            expect(result.body.Similar.Info.length).to.be.gt(0);
+            expect(result.body.Similar.Info[0].Name.toLowerCase()).to.be.equal(NewSearch[2].name.toLowerCase());
+            expect(result.body.Similar.Info[0].Name.toLowerCase()).contains(NewSearch[2].name.toLowerCase());
+//expect(result.body.Similar.Results.length).to.be.eq(NewSearch[0].limit);
             expect(result.body.Similar.Results.length).to.have.at.most(NewSearch[2].limit);
         })
     })
